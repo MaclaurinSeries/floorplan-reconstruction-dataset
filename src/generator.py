1f20e72f79372c6909bed7523f3e9d3e8b4cef23
MaclaurinSeries <63801107+MaclaurinSeries@users.noreply.github.com>
2022-10-20 20:07:52 +0700

    floorplan boundingbox and 2 floor generator

--- src/generator.py
+++ src/generator.py
@@ -1,19 +1,26 @@
-import sys, getopt, os
+import sys, getopt, os, io
+import numpy as np
+from PIL import Image
 from bs4 import BeautifulSoup
 from cairosvg import svg2png
-from image_util import remake_image, open_rgb, open_rgba
+from image_util import remake_image, open_rgb, open_rgba, get_bounding_box
+import cv2
+
+import matplotlib.pyplot as plt
 
 def main(argv):
-    input_directory = 'input'
-    output_directory = '.'
-    style_directory = 'style.css'
-    background_directory = 'bg'
-    texture_directory = 'txt'
+    input_directory = '.\\input'
+    output_directory = '.\\output'
+    asset_directory = '.\\asset'
     
-    help_text = 'generator.py -i <input_directory> -o <output_directory> -s <style_directory> -b <background_directory> -t <texture_directory>'
+    help_text = 'generator.py -i <input_directory> -o <output_directory> -a <asset_directory>'
     
     try:
-        opts, args = getopt.getopt(argv,"h:i:o:s:b:t:",["help=","idir=","odir=","style=","background=","texture="])
+        opts, args = getopt.getopt(argv,"h:i:o:a:",[
+                                   "help=",
+                                   "input-dir=",
+                                   "output-dir=",
+                                   "asset-dir="])
     except getopt.GetoptError:
         print(help_text)
         sys.exit(2)
@@ -22,34 +29,101 @@ def main(argv):
         if opt in ("-h", "--help"):
             print(help_text)
             sys.exit()
-        elif opt in ("-i", "--idir"):
+        elif opt in ("-i", "--input-dir"):
             input_directory = arg
-        elif opt in ("-o", "--odir"):
+        elif opt in ("-o", "--output-dir"):
             output_directory = arg
-        elif opt in ("-s", "--style"):
-            style_directory = arg
-        elif opt in ("-b", "--background"):
-            style_directory = arg
-        elif opt in ("-t", "--texture"):
-            style_directory = arg
-        
+        elif opt in ("-s", "--asset-dir"):
+            asset_directory = arg
     
     for folder in os.scandir(input_directory):
-        stop = False
         if folder.is_dir():
             for filename in os.scandir(folder.path):
                 if filename.is_file() and filename.name.endswith(".svg"):
-                    reformat_file(filename.path, style_directory, output_directory, os.path.splitext(filename.name)[0])
-                    stop = True
+                    process_image(filename.name, asset_directory, output_directory)
         elif folder.is_file() and folder.name.endswith(".svg"):
-            reformat_file(folder.path, style_directory, output_directory, os.path.splitext(folder.name)[0])
-            stop = True
+            process_image(folder.name, asset_directory, output_directory)
+
+def process_image(os_filename, asset_dir, out_dir):
+    # style_number = np.random.randint(0, 6)
+    style_number = 6
+    
+    bounding_box = {}
+    
+    with open(os_filename) as fsvg:
+        svg = fsvg.read()
+        with open(f'{asset_dir}\\style\\style_{style_number}.css') as fstyle:
             
-        if stop: break
+            soup = BeautifulSoup(svg, "xml")
+            
+            floorplans = soup.select("g[class=Floor]")
+
+            if (len(floorplans) > 2):
+                return None
+            
+            tag = soup.new_tag("style")
+            tag.string = fstyle.read()
+            soup.svg.g.insert_before(tag)
+
+            for floorplan in floorplans:
+                for s in floorplan.select('text'):
+                    s.extract()
+                
+                floorplan['style'] = ""
+                
+                svg = str(soup)
+                bb = extract_BB(svg)
+                
+                bounding_box[floorplan.g.get('id')] = bb
+                
+                floorplan['style'] = "display: none;"
+            
+            for floorplan in floorplans:
+                floorplan['style'] = ""
+                
+        svg = str(soup)
         
+    mem = io.BytesIO()
+    svg2png(bytestring=svg, write_to=mem)
+    img = np.array(Image.open(mem))
+    
+    bg = open_rgb(f'{asset_dir}\\background\\bg_{style_number}.jpg')
+    nimg, translation = remake_image(img, bg, scale=(1.3, 1.5), shift=(0.1, 0))
+    
+    # saving image with bounding box
+    for key,value in bounding_box.items():
+        trans = np.dot(translation, value).astype(int)
+        bounding_box[key] = trans
+        trans = trans.T
+        cv2.rectangle(nimg, trans[0], trans[1], (0,0,255), 1)
+        cv2.putText(nimg, key, trans[0], 0, 0.5, (0,0,255), 1//3)
+        
+    cv2.imwrite('saved.png', nimg[:,:,::-1])
+
+def extract_BB(svg):
+    mem = io.BytesIO()
+    svg2png(bytestring=svg, write_to=mem)
+    img = np.array(Image.open(mem))
+    
+    xl, yl, xr, yr = get_bounding_box(img)
+    
+#     return [
+#         [yr, yl, yl, yr, yr],
+#         [xl, xl, xr, xr, xl],
+#         [1, 1, 1, 1, 1]
+#     ]
+    return [
+        [yl, yr],
+        [xl, xr],
+        [1, 1]
+    ]
+
 def apply_style(svg, style):
     with open(style) as f:
         soup = BeautifulSoup(svg, "xml")
+        
+        if (len(soup.find_all("div", {"class": "Floorplan"})) > 2):
+            return None
 
         for s in soup.select('text'):
             s.extract()
@@ -60,19 +134,9 @@ def apply_style(svg, style):
         
         soup.svg.g.g.attrs["filter"] = "url(#texture)"
         
-        if (len(soup.find_all("div", {"class": "stylelistrow"})) >= 3) 
-        
         svg = str(soup)
         
     return svg
-    
-def reformat_file(image_path, style_dir, output_dir, filename):
-    with open(image_path) as f:
-        svg = apply_style(
-            f.read(),
-            style_dir)
-    
-    svg2png(bytestring = svg, write_to = f'{output_dir}\\{filename}.png')
             
 if __name__ == "__main__":
     main(sys.argv[1:])
--- src/image_util.py
+++ src/image_util.py
@@ -3,56 +3,61 @@ from PIL import Image
 import cv2
 
 def get_bounding_box(img):
+    if img.shape[2] > 3:
+        img = Image.fromarray(img)
+        imgwhite = Image.new("RGB", img.size, (255, 255, 255))
+        imgwhite.paste(img, mask=img.split()[3])
+        img = np.array(imgwhite)
+    
     x, y = np.where(np.all(img != [255, 255, 255], axis=2))
     return x[np.argmin(x)], y[np.argmin(y)], x[np.argmax(x)], y[np.argmax(y)]
 
-def resize_image(img, width=-1, height=-1):
+def resize_image(img, height=-1, width=-1):
     if width == -1 and height == -1:
         return img
     
-    if width == -1:
-        width = int(height * img.shape[0] / img.shape[1])
     if height == -1:
-        height = int(width * img.shape[1] / img.shape[0])
+        height = int(width * img.shape[0] / img.shape[1])
+    if width == -1:
+        width = int(height * img.shape[1] / img.shape[0])
     
     nimg = cv2.resize(img, dsize=(height, width), interpolation=cv2.INTER_CUBIC)
     
     return nimg
 
-def remake_image(img, bg, mask, scale=(1,1), shift=(0,0), W=-1, H=-1):
-    xl, yl, xr, yr = get_bounding_box(img)
+def remake_image(img, bg, scale=(1,1), shift=(0,0), W=-1, H=-1):
+    mask = img[:,:,3] > 0
     
     shp = img.shape
     nshp = (int(shp[1] * scale[1]),
             int(shp[0] * scale[0]))
     new_img = cv2.resize(bg, dsize=nshp, interpolation=cv2.INTER_CUBIC)
     
-    loc = (int((nshp[1] // 2) * (shift[0] + 1) - shp[0] // 2),
-           int((nshp[0] // 2) * (shift[1] + 1) - shp[1] // 2))
+    loc = (int((nshp[1] // 2 - shp[0] // 2) * (shift[0] + 1)),
+           int((nshp[0] // 2 - shp[1] // 2) * (shift[1] + 1)))
     
-    idx = (mask[xl : xr, yl : yr] == 1)
-    new_img[loc[0] : loc[0] + xr - xl,
-            loc[1] : loc[1] + yr - yl,
-            : ][idx] = img[xl : xr, yl : yr][idx]
+    new_img[loc[0] : loc[0] + shp[0],
+            loc[1] : loc[1] + shp[1],
+            : ][mask] = img[:,:,:3][mask]
+    
+    translation = np.array([
+        [1, 0, loc[1]],
+        [0, 1, loc[0]],
+    ])
     
     if W == -1 and H == -1:
-        return (new_img,
-                (loc[0],
-                 loc[1],
-                 loc[0] + xr - xl,
-                 loc[1] + yr - yl
-                ))
+        return new_img, translation
     
-    resized_img = resize_image(new_img, W, H)
+    resized_img = resize_image(new_img, H, W)
     H = resized_img.shape[0]
     W = resized_img.shape[1]
     
-    return (resized_img,
-            (int(loc[0] * W / nshp[0]),
-             int(loc[1] * H / nshp[1]),
-             int((loc[0] + xr - xl) * W / nshp[0]),
-             int((loc[1] + yr - yl) * H / nshp[1])
-            ))
+    translation = np.dot(np.array([
+        [H / nshp[0], 0],
+        [0, W / nshp[1]],
+    ]), translation)
+    
+    return resized_img, translation
 
 def open_rgb(path):
     img = Image.open(path)
@@ -69,3 +74,38 @@ def open_rgba(path):
     img = Image.open(path)
     
     return np.asarray(img)
+
+def similarity_feature(img1, img2):
+    MIN_MATCHES = 50
+    
+    img1 = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
+    img2 = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
+    
+    orb = cv2.ORB_create(nfeatures=500)
+
+    kp1, des1 = orb.detectAndCompute(img1,None)
+    kp2, des2 = orb.detectAndCompute(img2,None)
+    
+    index_params = dict(algorithm=6,
+                        table_number=6,
+                        key_size=12,
+                        multi_probe_level=2)
+    search_params = {}
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+    matches = flann.knnMatch(des1, des2, k=2)
+
+    good_matches = []
+    for m, n in matches:
+        if m.distance < 0.75 * n.distance:
+            good_matches.append(m)
+    
+    if len(good_matches) > MIN_MATCHES:
+        src_points = np.float32([kp1[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+        dst_points = np.float32([kp2[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+        m, mask = cv2.findHomography(src_points, dst_points, cv2.RANSAC, 5.0)
+        corrected_img = cv2.warpPerspective(img1, m, (img2.shape[1], img2.shape[0]))
+
+        return corrected_img
+    img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches[:50],None, flags=2)
+
+    return img3
